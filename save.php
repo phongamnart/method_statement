894f44e129f20e8a27709b72b5b124c57593190a
phongamnart <pknart17@gmail.com>
2024-05-16 16:19:10 +0700

    update

--- save.php
+++ save.php
@@ -7,19 +7,14 @@ use PhpOffice\PhpWord\IOFactory;
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $content = $_POST['editor_content'];
     
-    // สร้าง PHPWord object
     $phpWord = new PhpWord();
 
-    // เพิ่ม section
     $section = $phpWord->addSection();
 
-    // เพิ่มเนื้อหา HTML ลงใน section
     \PhpOffice\PhpWord\Shared\Html::addHtml($section, $content, false, false);
 
-    // ตั้งชื่อไฟล์ Word ที่จะบันทึก (เช่น ใช้วันที่และเวลา)
     $filename = "saved_files/" . date("Ymd_His") . ".docx";
 
-    // บันทึกไฟล์ Word
     $objWriter = IOFactory::createWriter($phpWord, 'Word2007');
     $objWriter->save($filename);
 
