a997791edcf47a1adca3597acd04e58edd653b1e
phongamnart <pknart17@gmail.com>
2024-05-21 14:07:56 +0700

    update

--- index.php
+++ index.php
@@ -4,9 +4,15 @@
     <meta charset="UTF-8">
     <title>CKEditor 5 Example</title>
     <script src="https://cdn.ckeditor.com/ckeditor5/41.4.2/classic/ckeditor.js"></script>
+    <style>
+        .ck-editor__editable[role="textbox"] {
+                /* Editing area */
+                min-height: 200px;
+            }
+    </style>
 </head>
 <body>
-    <form action="save.php" method="post">
+    <form action="test.php" method="post">
         <textarea name="editor_content" id="editor"></textarea>
         <br>
         <input type="submit" value="Save as Word">
@@ -17,6 +23,11 @@
                 ckfinder: {
                     uploadUrl: 'upload.php?command=QuickUpload&type=Files&responseType=json'
                 }
+            })
+            .then(editor => {
+                const editorElement = editor.ui.view.editable.element;
+                editorElement.style.height = '100%';
+                editorElement.style.width = '100%';
             })
             .catch(error => {
                 console.error(error);
--- upload.php
+++ upload.php
@@ -6,34 +6,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['upload'])) {
     $filename = uniqid() . '.' . $ext;
     $uploadPath = $uploadDirectory . $filename;
 
-    include("connect.php");
-
     if (move_uploaded_file($file['tmp_name'], $uploadPath)) {
         $url = 'http://' . $_SERVER['SERVER_NAME'] . '/method_statement/' . $uploadPath;
-
-        $stmt = $conn->prepare("insert into uploaded_files (filename, url) values (?, ?)");
-        $stmt->bind_param("ss", $filename, $url);
-        if($stmt->execute()){
-            $response = [
-                "uploaded" => 1,
-                "fileName" => $filename,
-                "url" => $url
-            ];
-        } else {
-            $response = [
-                "uploaded" => 0,
-                "error" => ["message" => "File to save file into to database."]
-            ];
-        }
-        $stmt->close();
+        $response = [
+            "uploaded" => 1,
+            "fileName" => $filename,
+            "url" => $url
+        ];
     } else {
         $response = [
             "uploaded" => 0,
             "error" => ["message" => "File upload failed."]
         ];
     }
-
-    $conn->close();
 
     echo json_encode($response);
 }
