b956059713602256b12b19659ea424258a7d1e78
phongamnart <pknart17@gmail.com>
2024-05-28 14:13:32 +0700

    update 2805

--- edit_doc.php
+++ edit_doc.php
@@ -6,31 +6,36 @@
     <title>CKEditor 5 Example</title>
     <script src="https://cdn.ckeditor.com/ckeditor5/41.4.2/classic/ckeditor.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
 </head>
 
 <body>
+    <?php
+    include("connect.php");
+    $conDB = new db_conn();
+
+    $id = isset($_GET['id']) ? $_GET['id'] : '';
+    $strSQLl =  "SELECT * FROM `documents` WHERE md5(`id`) = '$id' LIMIT 1";
+    $objQuery = $conDB->sqlQuery($strSQLl);
+
+    while ($objResult = mysqli_fetch_assoc($objQuery)) {
+        $tag_html = $objResult['tag_html'];
+    }
+
+    ?>
     <div class="container mt-3">
-        <div class="row">
-            <div class="d-flex justify-content-between align-items-center mb-3">
-                <button onclick="history.back()" class="btn btn-secondary me-2">Back</button>
-                <button class="btn btn-primary btn-home" onclick="location.href='index.php';">Home</button>
-            </div>
+        <div class="d-flex justify-content-between align-items-center mb-3">
+            <button class="btn btn-custom" onclick="history.back()" title="Back">
+                <i class="bi bi-arrow-left fs-4"></i>
+            </button>
+            <button class="btn btn-custom" onclick="location.href='index.php';" title="Home">
+                <i class="bi bi-house fs-4"></i>
+            </button>
+            <button class="btn btn-custom" onclick="window.location.href='edit_doc.php?id=<?php echo $id ?>'" title="Refresh">
+                <i class="bi bi-arrow-clockwise fs-4"></i>
+            </button>
         </div>
-
-        <?php
-        include("connect.php");
-        $conDB = new db_conn();
-
-        $id = isset($_GET['id']) ? $_GET['id'] : '';
-        $strSQLl =  "SELECT * FROM `documents` WHERE md5(`id`) = '$id' LIMIT 1";
-        $objQuery = $conDB->sqlQuery($strSQLl);
-
-        while ($objResult = mysqli_fetch_assoc($objQuery)) {
-            $tag_html = $objResult['tag_html'];
-        }
-
-        ?>
-
         <div class="row mt-3">
             <div class="col">
                 <form action="save_doc.php" method="post">
@@ -48,7 +53,7 @@
         ClassicEditor
             .create(document.querySelector('#editor'), {
                 ckfinder: {
-                    uploadUrl: "upload_img.php?id=" + documentId + "&command=QuickUpload&type=Files&responseType=json"
+                    uploadUrl: "upload_img.php?id=" + documentId + "&command=QuickUpload&type=Files&responseType=json" //ส่ง id ไปที่ upload_img.php
                 }
             })
             .catch(error => {
@@ -56,6 +61,7 @@
             });
     </script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
 </body>
 
 </html>
